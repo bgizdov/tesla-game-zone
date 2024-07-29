f3b100dcacf08a81f1f5c486df5ca3c8f7f0761f
Borislav Gizdov <borislav.gizdov@gmail.com>
2024-07-30 00:36:08 +0300

    Integrated react-paginate

--- Games/Blink_Beat/README.md
+++
@@ -1,8 +0,0 @@
-
-# Blink Beat
-A web-based game inspired by the classic Simon game, built using HTML, CSS, and JavaScript.
-
-
-## Live Website
-
-https://github.com/Shantnu-singh/Blink-Beat
--- src/App.js
+++ src/App.js
@@ -1,5 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import gamesData from './gamesData.json';
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import ReactPaginate from 'react-paginate';
+
+console.log('gamesData', gamesData);
+const items = gamesData.games;
+
+function Items({ currentItems }) {
+  return (
+    <>
+      {currentItems &&
+        currentItems.map((item) => (
+          <a href={`/Games/${item.gameUrl}`}>
+            <div>
+              <h3>{item.gameTitle}</h3>
+              <img src={`/images/${item.thumbnailUrl}`} width="200px" height="100px" alt={item.gameTitle} />
+            </div>
+          </a>
+        ))}
+    </>
+  );
+}
+
+function PaginatedItems({ itemsPerPage }) {
+  // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0);
+
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = items.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(items.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+      <Items currentItems={currentItems} />
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
+    </>
+  );
+}
+
+// Add a <div id="container"> to your HTML to see the component rendered.
+ReactDOM.render(
+  <PaginatedItems itemsPerPage={4} />,
+  document.getElementById('container')
+);
 
 function App() {
   return (
@@ -18,6 +85,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <div id="games"></div>
     </div>
   );
 }
